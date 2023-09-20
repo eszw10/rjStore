fd2c4c869f36f3488dba5b0ee9f66ac4b6da2fdd
eryc_s.z.w <erycszw10@gmail.com>
2023-09-20 12:07:58 +0700

    fix folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: [
+    'index.html',
+    './src/**/*.html'
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        'inter':['Inter', 'sans-serif'],
+        'poppins':['Poppins', 'sans-serif']
+      },
       colors: {
         'primary' : '#1F4644'
       }
