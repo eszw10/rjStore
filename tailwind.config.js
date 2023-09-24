a8d0eeb8bbd1f390579bfba5a026ba18102ab29a
eryc_s.z.w <erycszw10@gmail.com>
2023-09-24 14:26:56 +0700

    finish home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,21 @@ module.exports = {
         'primary' : '#1F4644',
         'secondary' : '#252F45',
         'dark': '#111827'
-      }
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [
